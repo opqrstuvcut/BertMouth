7f539832cd42ce45f066dff1a8eea30e5d20887d
tetsuya_hasegawa <vikz2713@gmail.com>
2019-10-21 09:15:07 +0000

    add preprocess

---
+++ preprocess.py
@@ -0,0 +1,36 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+import argparse
+from pyknp import Juman
+import mojimoji
+import tqdm
+
+
+def parse_argument():
+    parser = argparse.ArgumentParser("", add_help=True)
+    parser.add_argument("--input_file", required=True, type=str)
+    parser.add_argument("--output_file", required=True, type=str)
+    parser.add_argument("--option", type=str)
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = parse_argument()
+
+    if args.option:
+        option = args.option
+    else:
+        option = None
+
+    jumanpp = Juman(option=option)
+    with open(args.input_file, "r") as r, \
+            open(args.output_file, "w") as w:
+        for row in tqdm.tqdm(r):
+            result = jumanpp.analysis(mojimoji.han_to_zen(row.strip()))
+            w.write(" ".join([mrph.midasi for mrph in result.mrph_list()])
+                    + "\n")
+
+
+if __name__ == '__main__':
+    main()
