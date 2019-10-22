c04acdb29f2b3220edcfa24cdea78d8f7bc1c01d
tetsuya_hasegawa <vikz2713@gmail.com>
2019-10-22 12:41:57 +0000

    fix option argument

--- preprocess.py
+++ preprocess.py
@@ -10,7 +10,7 @@ def parse_argument():
     parser = argparse.ArgumentParser("", add_help=True)
     parser.add_argument("--input_file", required=True, type=str)
     parser.add_argument("--output_file", required=True, type=str)
-    parser.add_argument("--option", type=str)
+    parser.add_argument("--model", type=str)
     args = parser.parse_args()
     return args
 
@@ -18,8 +18,8 @@ def parse_argument():
 def main():
     args = parse_argument()
 
-    if args.option:
-        option = args.option
+    if args.model:
+        option = "--model=" + args.model
     else:
         option = None
 
@@ -27,7 +27,11 @@ def main():
     with open(args.input_file, "r") as r, \
             open(args.output_file, "w") as w:
         for row in tqdm.tqdm(r):
-            result = jumanpp.analysis(mojimoji.han_to_zen(row.strip()))
+            row = row.strip()
+            if not row:
+                continue
+            row = row.replace(" ", "")
+            result = jumanpp.analysis(mojimoji.han_to_zen(row))
             w.write(" ".join([mrph.midasi for mrph in result.mrph_list()])
                     + "\n")
 
